93315adaf04243a670c45c91499ce730e235348f
namitkapoor <namit.kapoor.21@dartmouth.edu>
2024-12-17 13:11:26 -0500

    Restored original vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,21 @@ import { transformWithEsbuild } from 'vite'
 import restart from 'vite-plugin-restart'
 
 export default({
-    root: 'src/', // This should point to where index.html is
+    root: 'src/',
     publicDir: '../public/',
-    plugins: [
-        // React support with Fast Refresh enabled
-        react({
-            fastRefresh: true
-        }),
-
+    plugins:
+    [
         // Restart server on static/public file change
-        restart({ restart: [ '../public/**' ] }),
+        restart({ restart: [ '../public/**', ] }),
+
+        // React support
+        react(),
 
         // .js file support as if it was JSX
         {
             name: 'load+transform-js-files-as-jsx',
-            async transform(code, id) {
+            async transform(code, id)
+            {
                 if (!id.match(/src\/.*\.js$/))
                     return null
 
@@ -28,27 +28,21 @@ export default({
             },
         },
     ],
-    server: {
-        host: true,
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env),
-        hmr: {
-            overlay: true,
-            timeout: 2000
-        },
-        watch: {
-            usePolling: true,
-            interval: 100
-        }
+    server:
+    {
+        host: true, // Open to local network and display URL
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
     },
-    build: {
-        outDir: '../dist',
-        emptyOutDir: true,
-        sourcemap: true,
+    build:
+    {
+        outDir: '../dist', // Output in the dist/ folder
+        emptyOutDir: true, // Empty the folder first
+        sourcemap: true, // Add sourcemap
         rollupOptions: {
             input: {
-                main: '/index.html'  // This should be relative to the root
+                main: '/index.html'  // Specify your entry point
             }
         }
     },
-    base: '/'
+    base: '/' // Add this to handle routing properly
 })
