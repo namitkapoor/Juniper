8aaace3225566718a6a1e35d7cac3949b02f0e57
namitkapoor <namitkapoor26@gmail.com>
2025-03-31 00:04:07 -0400

    space scene

--- src/Pages/Void.jsx
+++ src/Pages/Void.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import VoidExperiments from '../components/VoidExperiments';
+// import VoidExperiments from '../components/VoidExperiments';
 import Navbar from '../components/Navbar';
 import Contact from '../components/Contact';
-import FeaturedShowcase from '../components/FeaturedShowcase';
+// import FeaturedShowcase from '../components/FeaturedShowcase';
+import VoidScene from '../components/VoidScene';
 import '../style/void.css';
 
 export default function Void() {
     return (
         <div className="void-page">
             <Navbar />
-            <FeaturedShowcase />
-            <VoidExperiments />
+            <VoidScene />
+            {/* <FeaturedShowcase /> */}
+            {/* <VoidExperiments /> */}
             <Contact />
         </div>
     );
